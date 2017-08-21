2012644411f7bd04ca7c7f60998f7d6086fdb2ed
Miguel Angel Maldonado <desarrollo3@fincomun.com.mx>
2017-08-21 18:02:57 -0500

    Add validation for empty list

--- solution-assignment1/app.js
+++ solution-assignment1/app.js
@@ -9,6 +9,12 @@ function lunchCheckController($scope) {
 
 $scope.lunch= function () {
 
+if($scope.lunchs == undefined){
+	$scope.messStyle={color:'red'};
+	$scope.mess="Please enter data first";
+	return;
+}
+	
   var elemts = $scope.lunchs.split(",");
   $scope.mess='';
 
@@ -17,10 +23,11 @@ $scope.lunch= function () {
     $scope.messStyle={color:'red'};
       $scope.mess="Too much!";
   }
-    else {
+    else if( totalElemnts(elemts) > 0 && totalElemnts(elemts) <= 3) {
       $scope.messStyle={color:'green'};
       $scope.mess="Enjoy";
     }
+	
 }
 
 function totalElemnts(array) {
